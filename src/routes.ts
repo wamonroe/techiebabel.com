640608f62a645aca0c830b8c6278f24983a90b10
Alex Monroe <alex@monroepost.com>
2025-12-12 15:50:58 -0500

    Custom GitHub Pages 404
    Expand setup to include a custom 404 page designed to work with GitHub Pages.

--- src/routes.ts
+++ src/routes.ts
@@ -2,5 +2,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("./pages/Home.tsx"),
-  route("nimble", "./pages/Nimble.tsx")
+  route("nimble", "./pages/Nimble.tsx"),
+  route("*", "./pages/NotFound.tsx")
 ] satisfies RouteConfig;

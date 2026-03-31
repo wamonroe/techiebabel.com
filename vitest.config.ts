22f363ec9cf5d4b6902ceb686c59b3bafbb5e230
Alex Monroe <alex@monroepost.com>
2026-03-31 15:29:15 -0400

    Js updates latest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,7 @@
-import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "src")
